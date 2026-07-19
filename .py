b3260f3e7dcb61c060323d5a2e34fdda80747da5
vicky0700018 <vs2734514@gmail.com>
2026-07-19 22:20:27 +0530

    first commit

---
+++ .py
@@ -0,0 +1,13 @@
+class Rahul():
+    Rahul_fav_fruits = "Mango"
+
+Rahul_object = Rahul()
+print(Rahul_object.Rahul_fav_fruits)
+
+# Example 2
+
+class Riya():
+    Riya_fav_color = "Black"
+
+Riya_object = Riya()
+print(Riya_object.Riya_fav_color)
